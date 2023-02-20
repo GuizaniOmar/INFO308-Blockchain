362f679e4874d08948b073eb99ff2f8ea07f7c26
GuizaniOmar <omar.guizani@ulb.be>
2023-02-20 16:33:37 +0100

    Ajout du RSA

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,24 @@
 
 public class Main {
     public static void main(String[] args) {
-        RSA testing = new RSA();
-        byte[] testencrypter;
-        String testdecrypter = new String("");
-        testencrypter = testing.Encrypt("Coucou on va encrypté");
-        System.out.println(new String(testing.Encrypt("Coucou on va encrypté")));
-        testdecrypter  = testing.Decrypt(testencrypter);
-        System.out.println("Résultat du texte décrypté:");
-        System.out.println(testdecrypter);
+
+            try{
+                RSA rsa = new RSA();
+
+                String message = "Bonjour";
+                System.out.println("Message à encoder: " + message);
+
+                String encoded = rsa.encode(message, rsa.privateKey);
+                System.out.println("Message encodé: " + encoded);
+
+                String decoded = rsa.decode(encoded, rsa.publicKey);
+                System.out.println("Message décodé: " + decoded);
+            }catch(Exception e){
+            System.out.println("Exception produite !");
+        }
+
+
+
 
     }
 }
--- src/RSA.java
+++ src/RSA.java
@@ -1,48 +1,43 @@
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import javax.crypto.Cipher;
+import java.util.Base64;
 
 public class RSA {
-    KeyPairGenerator keyGen;
-    KeyPair pair;
-    Cipher cipher;
-    byte[] encrypted;
 
-    public RSA(){
-        try{
-            this.keyGen = KeyPairGenerator.getInstance("RSA");
-            this.keyGen.initialize(1024);
-            this.pair = this.keyGen.generateKeyPair();
-            this.cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-            System.out.println(this.pair.getPrivate());
-            System.out.println(this.pair.getPublic());
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
+    private static final String ALGORITHM = "RSA";
+    private static KeyPairGenerator keyGen;
+    private static KeyPair pair;
+    public static PrivateKey privateKey;
+    public static PublicKey publicKey;
+
+    // Attention ici publickey et privatekey sont public ! On va devoir les générer et les stocker (dans la fonction main) En mode:  Au lancement du programme si le fichier clef.txt n'est pas présent, générer les 2 clefs)
+
+    public RSA() throws Exception {
+        keyGen = KeyPairGenerator.getInstance(ALGORITHM);
+        keyGen.initialize(2048);
+        pair = keyGen.generateKeyPair();
+        privateKey = pair.getPrivate();
+        publicKey = pair.getPublic();
     }
 
-    public byte[] Encrypt(String plainText) {
-        try {
-            this.cipher.init(Cipher.ENCRYPT_MODE, this.pair.getPrivate());
-            this.encrypted = cipher.doFinal(plainText.getBytes());
-        }
-        catch (Exception e) {
-            System.out.println(e);
-        }
-        return this.encrypted;
+    public static String encode(String message, PrivateKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encoded = cipher.doFinal(message.getBytes());
+        return Base64.getEncoder().encodeToString(encoded);
     }
 
-    public String Decrypt(byte[] encrypted) {
-        String decryptedText = "";
-        try {
-            this.cipher.init(Cipher.DECRYPT_MODE, this.pair.getPublic());
-            decryptedText = new String(cipher.doFinal(encrypted));
-        }
-        catch (Exception e) {
-            System.out.println(e);
-        }
-        return decryptedText;
+    public static String decode(String message, PublicKey key) throws Exception {
+        byte[] decoded = Base64.getDecoder().decode(message);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decrypted = cipher.doFinal(decoded);
+        return new String(decrypted);
     }
+
+
 
 }
